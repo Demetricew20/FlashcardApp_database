e1d665c277de5f04b10afccfdd90a9b94ab7f877
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-03 08:39:35 -0800

    Fixed error within app admin and project urls.

--- flashcards/flashcard/urls.py
+++ flashcards/flashcard/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.CollectionList.as_view()),
     path('collection/<int:pk>', views.CollectionDetail.as_view()),
-    path('/flashcards', views.FlashcardList.as_view()),
-    path('/flashcards/<int:pk>', views.FlashcardDetails.as_view())
+    path('flashcards/', views.FlashcardList.as_view()),
+    path('flashcards/<int:pk>', views.FlashcardDetails.as_view())
 ]
